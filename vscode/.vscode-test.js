6e792b0f7e3ca0b03a1bcfc2f3c8c0b2aebba8fc
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2026-02-18 19:35:04 +0100

    Re-install extension

--- vscode/.vscode-test.js
+++ vscode/.vscode-test.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('@vscode/test-cli');
 
 module.exports = defineConfig([
 	{
-		//installExtensions: ['redhat.java@prerelease'],
+		installExtensions: ['redhat.java@prerelease'],
 		workspaceFolder: 'test/projects/small',
 		files: 'out/test/**/*.test.js',
 		mocha: {
