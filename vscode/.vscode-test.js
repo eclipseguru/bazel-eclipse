f3f674b10072b9949cc6d922a8420ee608ab9496
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2026-02-18 18:54:40 +0100

    Fix test

--- vscode/.vscode-test.js
+++ vscode/.vscode-test.js
@@ -2,21 +2,21 @@
 const { defineConfig } = require('@vscode/test-cli');
 
 module.exports = defineConfig([
-  {
-    installExtensions: ['redhat.java@prerelease'],
-    workspaceFolder: 'test/projects/small',
-    files: 'out/test/**/*.test.js',
-    mocha: {
-      ui: 'tdd',
-      timeout: 60000,
-      reporter: 'json',
-      reporterOptions: {
-        output: './test/result/extension.test.json',
-        reportDir: './test/result',
-        reportFilename: "[status]_[datetime]-[name]-report",
-        reportTitle: 'Bazel Eclipse Extension Tests',
-        reportPageTitle: 'Bazel Eclipse Extension Tests',
-      },
-    }
-  }
+	{
+		//installExtensions: ['redhat.java@prerelease'],
+		workspaceFolder: 'test/projects/small',
+		files: 'out/test/**/*.test.js',
+		mocha: {
+			ui: 'tdd',
+			timeout: 60000,
+			reporter: 'json',
+			reporterOptions: {
+				output: './test/result/extension.test.json',
+				reportDir: './test/result',
+				reportFilename: '[status]_[datetime]-[name]-report',
+				reportTitle: 'Bazel Eclipse Extension Tests',
+				reportPageTitle: 'Bazel Eclipse Extension Tests',
+			},
+		},
+	},
 ]);
